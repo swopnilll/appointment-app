414a0dec278d2494f40832856131d4c05a34b3a6
swopnilll <swopnil@lims1.com>
2024-12-09 14:38:30 +1100

    Basic Create and Delete

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Appointment } from '../models/appointment';
+
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  
+  newAppointmentTitle: string = ""
+  newAppointmentDate: Date = new Date();
+  
+  appointments: Appointment[] = []
 
+  addAppointment(){
+    if(this.newAppointmentDate && this.newAppointmentTitle.trim().length){
+      this.appointments = [...this.appointments, {
+        id: Date.now(),
+        title: this.newAppointmentTitle.trim(),
+        date: this.newAppointmentDate
+      }]
+    
+      this.newAppointmentDate = new Date();
+      this.newAppointmentTitle = "";   
+    }
+  }
+
+  deleteAppointment(id : number){
+    this.appointments = [...this.appointments.filter(appointment => appointment.id !== id)];
+  }
+  
 }
